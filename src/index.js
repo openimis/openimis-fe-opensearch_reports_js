ef55bf3f2958d41c21e547c9d04fcbec000e0cad
sniedzielski <sniedzielski@soldevelo.com>
2023-06-22 14:08:02 +0200

    CM-152: fixed estlint

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const DEFAULT_CONFIG = {
   translations: [{ key: 'en', messages: messagesEn }],
 };
 
-// Disable due to core architecture
-/* eslint-disable Prefer default export on a file with single export */
+// Disable due to core architecture - Prefer default export on a file with single export
+/* eslint-disable */
 export const OpensearchReportsModule = (cfg) => ({ ...DEFAULT_CONFIG, ...cfg });
