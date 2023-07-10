d81e6447d5fbe162599fe96195435f74fbe100c8
sniedzielski <52816247+sniedzielski@users.noreply.github.com>
2023-07-10 17:04:13 +0200

    CM-153: changed default proxyHost (#4)
    * CM-153: changed default proxyHost
    
    * CM-153: updated README
    
    * CM-153: changing in iframe - taking hostname from browser

--- src/components/OpenSearchDashboard.js
+++ src/components/OpenSearchDashboard.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 /* eslint-disable */
 function OpenSearchDashboard() {
-  const proxyHost = process.env.PROXY_HOST ?? '172.20.20.13';
+  const currentHostname = window.location.hostname;
   const openSearchBaseRootPath = process.env.OPENSEARCH_PROXY_ROOT ?? 'opensearch';
 
   return (
     <iframe
-      src={`https://${proxyHost}/${openSearchBaseRootPath}/`}
+      src={`https://${currentHostname}/${openSearchBaseRootPath}/`}
       title="OpenSearch Dashboard" // Add a unique title property
       style={{ position: 'absolute', width: '95%', height: '90%' }}
       allow="same-origin allow-scripts"
