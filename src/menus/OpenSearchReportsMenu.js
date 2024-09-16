0a1330de06c0facf369cd721d971b42ce00e5e62
sniedzielski <sniedzielski@soldevelo.com>
2024-09-16 14:18:35 +0200

    CM-998: fixed eslint

--- src/menus/OpenSearchReportsMenu.js
+++ src/menus/OpenSearchReportsMenu.js
@@ -4,10 +4,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { injectIntl } from 'react-intl';
-import { Tune, DoubleArrow, Person, People, Update } from '@material-ui/icons';
+import {
+  Tune,
+  DoubleArrow,
+  Person,
+  People,
+  Update,
+} from '@material-ui/icons';
 import PaymentIcon from '@material-ui/icons/Payment';
 import ToolIcon from '@material-ui/icons/Build';
-import { formatMessage, MainMenuContribution, withModulesManager } from '@openimis/fe-core';
+import {
+  formatMessage,
+  MainMenuContribution,
+  withModulesManager,
+} from '@openimis/fe-core';
 import { OPENSEARCH_REPORTS_MAIN_MENU_CONTRIBUTION_KEY } from '../constants';
 
 function OpenSearchReportsMenu(props) {
