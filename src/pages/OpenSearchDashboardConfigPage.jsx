ec8696d6dd38ce9b5a711cd2901ec9fb1e333cab
olewandowski1 <olewandowski@soldevelo.com>
2026-02-03 16:14:14 +0100

    fix: fix typo

--- src/pages/OpenSearchDashboardConfigPage.jsx
+++ src/pages/OpenSearchDashboardConfigPage.jsx
@@ -1,11 +1,13 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import React from 'react';
-import { Helmet, withModulesManager, formatMessage } from '@openimis/fe-core';
-import { injectIntl } from 'react-intl';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { styled } from '@mui/material/styles';
+import { formatMessage, Helmet, withModulesManager } from '@openimis/fe-core';
+import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
 import OpenSearchDashboardTable from '../components/tables/OpenSearchDashboardTable';
 
-const StyledOpenSearchDashboardConfigPages = styled('div')(({ theme }) => ({
+const StyledOpenSearchDashboardConfigPage = styled('div')(({ theme }) => ({
   ...(theme?.page ?? {}),
   '& .fab': theme?.fab ?? {},
 }));
@@ -14,14 +16,23 @@ function OpenSearchDashboardConfigPage(props) {
   const { intl } = props;
   return (
     <StyledOpenSearchDashboardConfigPage>
-      <Helmet title={formatMessage(intl, 'openSearchReports', 'openSearch.openSearchConfig')} />
+      <Helmet
+        title={formatMessage(
+          intl,
+          'openSearchReports',
+          'openSearch.openSearchConfig',
+        )}
+      />
       <OpenSearchDashboardTable />
     </StyledOpenSearchDashboardConfigPage>
   );
 }
 
 const mapStateToProps = (state) => ({
-  rights: !!state.core && !!state.core.user && !!state.core.user.i_user ? state.core.user.i_user.rights : [],
+  rights:
+    !!state.core && !!state.core.user && !!state.core.user.i_user
+      ? state.core.user.i_user.rights
+      : [],
 });
 
 export { StyledOpenSearchDashboardConfigPage };
