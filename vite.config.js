f6bc825837ebe5823e390c4a023e26d9c36542ba
olewandowski1 <olewandowski@soldevelo.com>
2026-02-12 12:03:16 +0100

    fix: use regex for MUI/Emotion in rollupOptions.external

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,6 @@ export default defineConfig({
       external: [
         /^@babel.*/,
         /^@date-io\/.*/,
-       
         /^@openimis.*/,
         'classnames',
         'clsx',
@@ -25,12 +24,12 @@ export default defineConfig({
         'prop-types',
         /^react.*/,
         /^redux.*/,
-        '@mui/material',
-        '@mui/icons-material',
+        /^@mui\/material/,
+        /^@mui\/icons-material/,
         '@mui/x-date-pickers',
-        '@emotion/react',
-        '@emotion/styled',
-        '@emotion/cache',
+        /^@emotion\/react/,
+        /^@emotion\/styled/,
+        /^@emotion\/cache/,
         '@mui/styled-engine',
       ],
     //   plugins: [json()]
